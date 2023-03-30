5d14de5bbe2bb4e4db5d9f36cc5454df93a086a4
segolene <segolene.ganzin@gmail.com>
2023-03-30 19:20:32 +0200

    30-03-23 Ajout du correctif de fichier sur le routage

---
+++ controleur/Routage.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *	Module du routing (routage).
+ *	Chaque action est récupérée par la méthode : $_GET
+ */
+
+function redirigeVers($action = "defaut")
+{
+
+    $lesActions = [];
+    $lesActions["defaut"] = "TestC.php";
+    $lesActions["connexion"] = "Authentification.php";
+    $lesActions["deconnexion"] = "Deconnexion.php";
+    $lesActions["praticien"] = "Praticien.php";
+    $lesActions["accueil-praticien"] = "AccueilPraticien.php";
+    $lesActions["patient"] = "Patient.php";
+    $lesActions["inscription"] = "Inscription.php";
+    $lesActions["accueil"] = "TestC.php";
+    $lesActions["fiche-patient"] = "FichePatient.php";
+
+    $controler_id = $lesActions[$action];
+
+    //si le fichier n'existe pas :
+    if (!file_exists(__DIR__ . '/' . $controler_id)) die("Le fichier : " . $controler_id . " n'existe pas !");
+
+    //si la clé "action" existe dans notre tableau "lesActions" :
+    if (array_key_exists($action, $lesActions)) {
+        // le fichier à inclure sera retourné :
+        return $controler_id;
+    } else {
+        return $lesActions["defaut"];
+    }
+}
